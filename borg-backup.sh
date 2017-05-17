f7ce4a53ab1f30bce197f74ff56fc40250266612
Lele <lele@sshadm.in>
2017-05-17 23:26:58 +0200

    Fixed boolean logic in BORG_SKIP_CHECK variable
    Fixed boolean logic in BORG_SKIP_CHECK variable usage, on my own commit.
    
    Sorry for the double commit.

--- borg-backup.sh
+++ borg-backup.sh
@@ -126,7 +126,7 @@ if [ -n "${PRUNE:-}" ]; then
     borg prune -v --stats --show-rc $PRUNE_PREFIX --keep-daily=$KEEP_DAILY --keep-weekly=$KEEP_WEEKLY --keep-monthly=$KEEP_MONTHLY
 fi
 
-if [ "${BORG_SKIP_CHECK:-}" != '1' ] || [ "${BORG_SKIP_CHECK:-}" != "true" ]; then
+if [ "${BORG_SKIP_CHECK:-}" != '1' ] && [ "${BORG_SKIP_CHECK:-}" != "true" ]; then
     borg check -v --show-rc
 fi
 
