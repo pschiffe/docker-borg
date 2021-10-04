a7463367370091fd45306adbd5408d70df8c6099
Peter Schiffer <3899107+pschiffe@users.noreply.github.com>
2021-10-04 23:14:03 +0200

    Use ed25519 key instead of rsa when generating ssh key
    Related #10

--- borg-backup.sh
+++ borg-backup.sh
@@ -21,7 +21,7 @@ function quit {
 if [ -n "${SSHFS:-}" ]; then
     if [ -n "${SSHFS_IDENTITY_FILE:-}" ]; then
         if [ ! -f "$SSHFS_IDENTITY_FILE" ] && [ -n "${SSHFS_GEN_IDENTITY_FILE:-}" ]; then
-            ssh-keygen -t rsa -b 4096 -N '' -f "$SSHFS_IDENTITY_FILE"
+            ssh-keygen -t ed25519 -N '' -f "$SSHFS_IDENTITY_FILE"
             cat "${SSHFS_IDENTITY_FILE}.pub"
             exit 0
         fi
